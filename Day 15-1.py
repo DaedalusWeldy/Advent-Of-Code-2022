6501abe191194e868c571774d9963f775dbb0eb1
DaedalusWeldy <daniel.r.weldy@gmail.com>
2024-11-13 16:28:27 -0800

    Added method for reading coordinates from list

--- Day 15-1.py
+++ Day 15-1.py
@@ -1,10 +1,30 @@
 # Advent of Code 2022 - Day 15 Part 1
 # Sensors and beacons, and a specific y-level? 
 
+import re
+
+# Read and convert text list of coordinates into dicts
+# Dicts will contain 'sensor' and 'beacon' keys, the values of which 
+# will be in [X, Y] format.
+def read_coords_list(file_input):
+    coords_to_return = []
+    for line in file_input:
+        split_line = line.strip("\n")
+        split_line = re.sub("[,:]", "", split_line)
+        split_line = re.split("\s", split_line)
+        split_line = [split_line[2], split_line[3], split_line[8], split_line[9]]
+        read_sensor = [int(split_line[0][2:]), int(split_line[1][2:])]
+        read_beacon = [int(split_line[2][2:]), int(split_line[3][2:])]
+        sensor_beacon_pair = {"sensor": read_sensor, "beacon": read_beacon}
+        coords_to_return.append(sensor_beacon_pair)
+    
+    return coords_to_return
+
+
 # Merge two lists, skipping duplicate values
 def combine_lists(original_list, list_to_join):
     for entry in list_to_join:
-        if original_list.count(entry) == 0:
+        if entry not in original_list:
             original_list.append(entry)
     original_list.sort()
 
@@ -42,3 +62,9 @@ def generate_mark_list(target_y_level, sensor_coord, beacon_coord):
     # sort the list and return it
     list_to_return = list_to_return.sort()
     return list_to_return
+
+
+# TEST AREA
+with open("Scratch Files/Day 15 Test.txt", "r") as test_input:
+    coord_list = read_coords_list(test_input)
+    print(coord_list)
